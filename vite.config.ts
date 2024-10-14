64fb20464eb8ac0fb52825bdc182c5e5bc061775
dovkarash <50068016+dovkarash@users.noreply.github.com>
2024-10-14 10:04:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
 import { URL, fileURLToPath } from 'node:url'
-
-import { createRequire } from 'node:module'
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+// import basicSSL from '@vitejs/plugin-basic-ssl'
 import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
-const _require = createRequire(import.meta.url)
 
-const env = loadEnv('', _require('process').cwd())
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		// basicSSL(),
 		vue(),
 		ReactivityTransform(),
-		vueJsx(),
+		vueDevTools(),
 	],
 	resolve: {
 		alias: {
